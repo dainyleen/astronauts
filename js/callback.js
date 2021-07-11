2629f5e436833ec9afa6c11f884b828cb556cf95
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-11 18:53:36 +1200

    Added function getJSON to retrieve data from a server via url

--- js/callback.js
+++ js/callback.js
@@ -6,3 +6,16 @@ const btn = document.querySelector('button')
 const astrosUrl = 'http://api.open-notify.org/astros.json'
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
 
+// Make an AJAX request
+function getJSON(url) {
+  const ajax = new XMLHttpRequest()
+  ajax.open('GET', url)
+  ajax.onload = () => {
+    if (ajax.status === 200) {
+      let data = JSON.parse(ajax.responseText)
+      console.log(data)
+    }
+  }
+  ajax.send()
+}
+
