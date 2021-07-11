d59501966378d663319fb27826da09cc1bdea0e6
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-11 18:49:46 +1200

    Initialized variables for api endpoints

--- js/callback.js
+++ js/callback.js
@@ -1,3 +1,8 @@
 // Declare variables
 const peopleList = document.getElementById('people')
 const btn = document.querySelector('button')
+
+// Initialize variables for api endpoints
+const astrosUrl = 'http://api.open-notify.org/astros.json'
+const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+
