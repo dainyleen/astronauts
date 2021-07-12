81653aae1650a484d199a8827c41c5f009594a8c
Dainy <74221561+dainyleen@users.noreply.github.com>
2021-07-12 14:55:17 +1200

    Update callback.js

--- js/callback.js
+++ js/callback.js
@@ -3,7 +3,7 @@ const peopleList = document.getElementById('people')
 const btn = document.querySelector('button')
 
 // Initialize variables for api endpoints
-const astrosUrl = 'http://api.open-notify.org/astros.json'
+const astrosUrl = 'https://api.open-notify.org/astros.json'
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
 
 // Make an AJAX request
